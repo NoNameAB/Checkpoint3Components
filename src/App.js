1515b9c7239a88771682268e5eee9c42acaee801
NoNameAB <48261117+NoNameAB@users.noreply.github.com>
2019-06-25 15:23:33 +0100

    Checkpoint 3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import './style.css';
+import Container from './Components/Container';
+
+function App() {
+  return(
+    <div className = 'App'>
+	    <Container/>
+    </div>
+  )
+  
+}
+
+export default App;
